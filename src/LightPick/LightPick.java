d19289cf23a74343d36e9a2a73842f163c871b4e
Redmancometh <Brendanstcurry@gmail.com>
2014-06-24 13:58:39 -0600

    First Commit

---
+++ src/LightPick/LightPick.java
@@ -0,0 +1,17 @@
+package LightPick;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class LightPick extends JavaPlugin 
+{
+	public void onEnable()
+	{
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+	    pm.registerEvents(new Listeners(), this);
+	}
+	
+}
